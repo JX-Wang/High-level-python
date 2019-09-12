8f2e5db7a1671779b7f28831449f75735de7cdc0
jx-wang <1411349759@qq.com>
2019-09-12 19:11:42 +0800

    new sort code

--- Sort/ALL.py
+++ Sort/ALL.py
@@ -65,7 +65,8 @@ def insert_sort(array):
                     rst.insert(i, tmp)
                     flag = 1
                     break
-                else:pass
+                else:
+                    pass
             if not flag:
                 rst.append(tmp)
     return rst
@@ -75,8 +76,32 @@ def quick_sort(array):
     pass
 
 
-def heap_sort(array):
-    pass
+def max_heapify(heap, HeapSize, root):  # 在堆中做结构调整使得父节点的值大于子节点
+
+    left = 2*root + 1
+    right = left + 1
+    larger = root
+    if left < HeapSize and heap[larger] < heap[left]:
+        larger = left
+    if right < HeapSize and heap[larger] < heap[right]:
+        larger = right
+    if larger != root:  # 如果做了堆调整则larger的值等于左节点或者右节点的，这个时候做对调值操作
+        heap[larger], heap[root] = heap[root], heap[larger]
+        max_heapify(heap, HeapSize, larger)
+
+
+def build_max_heap(heap):  # 构造一个堆，将堆中所有数据重新排序
+    HeapSize = len(heap)  # 将堆的长度单独拿出来方便
+    for i in xrange((HeapSize - 2)//2, -1, -1):  # 从后往前出数
+        max_heapify(heap, HeapSize, i)
+
+
+def heap_sort(heap):  # 将根节点取出与最后一位做对调，对前面len-1个节点继续进行对调整过程。
+    build_max_heap(heap)
+    for i in range(len(heap)-1, -1, -1):
+        heap[0], heap[i] = heap[i], heap[0]
+        max_heapify(heap, i, 0)
+    return heap
 
 
 def shell_sort(array):
@@ -101,4 +126,4 @@ def radix_sort(array):
 
 if __name__ == '__main__':
     array = [3, 4, 2, 5, 1, 6, 9, 8, 7]
-    print insert_sort(array)
+    print heap_sort(array)
